a98901efbef5b30505fbfc4000e41f916851fda3
j7f3rn4nd3517 <jtfernandes.it@gmail.com>
2017-09-27 17:05:16 -0300

    Add files via upload
    Coursera - Python - USP

---
+++ convert_temp.py
@@ -0,0 +1,7 @@
+# Definindo variavel de entrada (Fahrenheit
+F = input("Digite uma temperatura em Fahrenheit: ")
+
+# Processamento da conversao de Fahrenheit para Celsius
+temperaturaCelsius = (float(F) - 32) * 5 / 9
+
+print ("A temperatura em Celsius é ", temperaturaCelsius)
---
+++ medias.py
@@ -0,0 +1,9 @@
+# Programa que recebe 4 notas e calcula a media aritimética
+n1 = float(input("Digite a primeira nota "))
+n2 = float(input("Digite a segunda nota "))
+n3 = float(input("Digite a terceira nota "))
+n4 = float(input("Digite a quarta nota "))
+
+media = (n1+n2+n3+n4) /4
+
+print("A média aritmética é", media)
